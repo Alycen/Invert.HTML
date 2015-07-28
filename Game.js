8847964f4fb62757ee8e0bc61ef2d23f34cef949
alycen <alsunnid@gmail.com>
2015-07-28 09:20:32 +0100

    added blank javascript files

---
+++ Game.js
@@ -0,0 +1,22 @@
+var game;
+
+var requestAnimFrame = (function() {
+    return window.requestAnimationFrame    ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+function Game() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innderHeight;
+}
+
+function main() {
+    game = new Game();
+    
+}
