25aec2b0637f8df06340a1b67a7ac56f2efcb11e
alycen <alsunnid@gmail.com>
2015-07-29 09:05:37 +0100

    Background folder, changes to player and level

--- Level.js
+++ Level.js
@@ -1,3 +1,16 @@
 function Level() {
-
+//    this.scale = 1;
+    this.isVisible = false;
+    this.bg = new Image();
+    this.bg.src = "assets/gfx/Background/lvl.png";
 }
+
+/*Level.prototype.getLevel = function() {
+        return this.lvl;
+}*/
+
+Level.prototype.Draw = function() {
+    game.ctx.drawImage(this.bg,0,0,game.screenWidth,game.screenHeight);
+}
+
+
--- Player.js
+++ Player.js
@@ -1,3 +1,28 @@
 function Player() {
+    this.x = 0;
+    this.y = 0;
+    this.width = 10;
+    this.height = 10;
+    this.isVisible = false;
+    this.boundingBox = new boundingBox(this.x,this.y.this.width,this.height);
+}
 
+Player.prototype.Update = function() {
+    
+}
+
+Player.prototype.getX = function() {
+    return this.x;
+}
+
+Player.prototype.getY = function() {
+    return this.y;
+}
+
+Player.prototype.setX = function(x) {
+    this.x = x;
+}
+
+Player.prototype.setY = function(y) {
+    this.y = y;
 }
