c30ec5a9eb238db727f15766af97b76611487c57
alycen <alsunnid@gmail.com>
2015-07-30 08:33:15 +0100

    small tweaks

--- Block.js
+++ Block.js
@@ -1,17 +1,21 @@
 var PLAYER = 0, WALL = 1, BOULDER = 2, PICKUP = 3;
 
-function Block() {
+function Block(type) {
     this.x = 0;
     this.y = 0;
     this.width = 10;
     this.height = 10;
     this.isVisible = false;
     this.boundingBox = new boundingBox(this.x,this.y.this.width,this.height);
-    this.type = -1;
+    this.type = type;
 }
 
 Block.prototype.Update = function() {
     
+}
+
+Block.prototype.Move() {
+
 }
 
 Block.prototype.getX = function() {
