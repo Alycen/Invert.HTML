3921bb5659d314079b39cee944947c88cc1e06ec
alycen <alsunnid@gmail.com>
2015-07-31 08:57:14 +0100

    changed initCanv againwq

--- Game.js
+++ Game.js
@@ -19,9 +19,9 @@ function Game() {
 
 function main() {
     game = new Game();
-    game.initCanvas();
     game.level = new Level(1);
     game.player = new Block(0);
+    game.initCanvas();
     game.touch.main();
     game.gameLoop();
 }
