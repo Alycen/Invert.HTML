1713993ffba1cbbbff56594d2867740ed3020554
root <root@amcdonald_cent7_vm.druidnetwork>
2015-08-11 08:46:31 +0100

    added a move method for blocks (Not tested, on wrong PC)

--- Block.js
+++ Block.js
@@ -18,8 +18,15 @@ Block.prototype.Draw = function() {
     game.ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
 }
 
-Block.prototype.Move = function() {
-
+Block.prototype.Move = function(dir) {
+    if (dir == 1) 	// UP
+	this.y -= 2;
+    else if (dir == 2)	// DOWN
+	this.y += 2;
+    if (dir == 3)	// LEFT
+	this.x -= 2;
+    else if (dir ==4)	// RIGHT
+	this.x += 2;
 }
 
 Block.prototype.setType = function(type) {
