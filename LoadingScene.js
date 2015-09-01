3a239fa59d071c104334ef3ab402e44fc97f91d9
alycen <alsunnid@gmail.com>
2015-09-01 08:00:23 +0100

    got loading screen moving

--- LoadingScene.js
+++ LoadingScene.js
@@ -2,6 +2,7 @@ function LoadingScene() {
 	this.bg.src = "assets/gfx/loading.png";
 	this.playerVisible  = false;
 	this.player = new Player(-100,-100,1,1);
+	this.i = 10;
 }
 
 LoadingScene.prototype = new BaseScene() 
@@ -14,5 +15,12 @@ LoadingScene.prototype.Wait = function() {
 }
 
 LoadingScene.prototype.Update = function() {
+	this.i --;
+	if(this.i == 0)
+		this.i = 10;
 	this.Wait();
+}
+
+LoadingScene.prototype.Draw = function() {
+	game.ctx.drawImage(this.bg,100 * this.i, 0, 100, 100, (game.screenWidth / 2) - 100, (game.screenHeight / 2) - 100, 200, 200);
 }
